2ee4d5e2fbd27eeafeb8e926e556bb0d552e1bca
toy <1659808224@qq.com>
2015-11-19 14:23:10 +0800

    233

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 var koa = require('koa')
-var route=require('koa-route')
-var robe=require('robe')
-var session = require('koa-generic-session')
-var MongoStore = require('koa-generic-session-mongo')
+var router = require('koa-router')()
+var logger = require('koa-logger')
+var serve = require('koa-static')
 var app = koa()
-var key=require('./key')
-app.keys = ['toy','key']
-app.use(session({store:new MongoStore()}))
-app.use(function*(next){
-	if(!global.db)global.db=yield robe.connect('localhost/toy')
-	yield next
-})
-app.use(route.get('/key/:id',key.getOne))
-app.use(route.get('/key',key.get))
-app.use(route.post('/key',key.post))
-app.use(route.get('/',function*(){
-	this.body="test"
-}))
-app.listen(80);
+router
+	.get('/get',function*(next){
+		this.body="get"
+	})
+	.post('/post',function*(next){
+		this.body="post"
+	})
+app
+	.use(logger())
+	.use(router.routes())
+	.use(router.allowedMethods())
+	.use(serve('public'))
+	.listen(80)
+console.log("start app success on port 80")
