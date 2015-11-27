bd353fc13c06f091f03a4412936d687d3cd312f2
toy <1659808224@qq.com>
2015-11-27 16:06:41 +0800

    test

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 var koa    = require('koa'),
 	router = require('koa-router')(),
 	logger = require('koa-logger'),
-	cache  = require('koa-static-cache'),
-	serve  = require('koa-static'),
 	mongo  = require('koa-mongo'),
-	path   = require('path')
+	serve  = require('koa-static'),
+	path   = require('path'),
+	parse  = require('co-body')
 var app = koa()
 router
-	.get('/get',function*(next){
-		this.body=yield this.mongo.collection('users').findOne()
+	.get('/data',function*(next){
+		this.body=yield this.mongo.collection('data').find().sort({time:1}).toArray()
 	})
-	.get('/post',function*(next){
-		this.body=yield this.mongo.collection('users').insert({a:2})
+	.post('/data',function*(next){
+		var data =(yield parse(this)).data
+		data=JSON.parse(data)
+		data.time=new Date()
+		this.body=yield this.mongo.collection('data').insert(data)
 	})
 app
 	.use(mongo({host:process.env["MONGODB_PORT_27017_TCP_ADDR"]||'localhost',port:process.env["MONGODB_PORT_27017_TCP_PORT"]||27017,user:process.env["MONGODB_USERNAME"]||'',pass:process.env["MONGODB_PASSWORD"]||' ',db:process.env["MONGODB_INSTANCE_NAME"]||'test'}))
 	.use(logger())
 	.use(router.routes())
 	.use(router.allowedMethods())
-	.use(cache('public'))
-	.use(serve('public'))
+	.use(serve("public"))
 	.listen(80)
 console.log("start app success on port 80")
