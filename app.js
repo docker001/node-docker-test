44fa5979e399c1d15d0eb50750fa74e16f751266
toy <1659808224@qq.com>
2015-12-06 17:15:16 +0800

    test

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ var koa    = require('koa'),
 	cache  = require('koa-static-cache'),
 	path   = require('path'),
 	parse  = require('co-body'),
-	session = require('koa-session-store')
+	session = require('koa-session-store'),
+	ObjectId = require('mongodb').ObjectId
 var app = koa()
 app.keys=['imtoy']
+var auth=function*(next){
+	if(!this.session.user||this.session.user.group!="admin")return this.body={error:"无权限"}
+	yield next
+}
 router
 	.get('/user/getInfo',function*(){
 		this.body=this.session.user||{error:"not login"}
@@ -23,7 +28,7 @@ router
 		this.session=null
 		this.body={status:"ok"}
 	})
-	.get('/user/init',function*(next){
+	.get('/user/init',function*(){
 		if(yield this.mongo.collection("users").findOne({account:"admin"}))return
 		var result={}
 		result.indexes=yield this.mongo.collection("users").ensureIndex({account:1},{unique: true})
@@ -31,7 +36,17 @@ router
 		result.creatTest=yield this.mongo.collection("users").insert({account:"test",password:"test",group:"user"})
 		this.body=result
 	})
-	.get('/config/init',function*(next){
+	.post('/news',auth,function*(){
+		this.body=yield this.mongo.collection("news").insert(yield parse.json(this))
+	})
+	.get('/news',function*(){
+		this.body=yield this.mongo.collection("news").find().sort({date:-1}).skip(parseInt(this.query.start)||0).limit(parseInt(this.query.limit)||100).toArray()
+	})
+	.get('/news/meta',function*(){
+		this.body={total:yield this.mongo.collection("news").count(),keys:["ID","标题","简介","日期"]}
+	})
+	.get('/news/del',auth,function*(){
+		this.body=yield this.mongo.collection("news").remove({_id:ObjectId(this.query.id)})
 	})
 app
 	.use(mongo({host:process.env["MONGODB_PORT_27017_TCP_ADDR"]||'localhost',port:process.env["MONGODB_PORT_27017_TCP_PORT"]||27017,user:process.env["MONGODB_USERNAME"]||'',pass:process.env["MONGODB_PASSWORD"]||' ',db:process.env["MONGODB_INSTANCE_NAME"]||'test'}))
