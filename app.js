c8f8687215815caec2f880a49f6d6a09166e49c4
toy <1659808224@qq.com>
2015-12-08 18:03:12 +0800

    test

--- app.js
+++ app.js
@@ -17,7 +17,13 @@ router
 	.use('/user',routerUser.routes(),routerUser.allowedMethods())
 	.use('/db',routerDB.routes(),routerDB.allowedMethods())
 app
-	.use(mongo({host:process.env["MONGODB_PORT_27017_TCP_ADDR"]||'localhost',port:process.env["MONGODB_PORT_27017_TCP_PORT"]||27017,user:process.env["MONGODB_USERNAME"]||'',pass:process.env["MONGODB_PASSWORD"]||' ',db:process.env["MONGODB_INSTANCE_NAME"]||'test'}))
+	.use(mongo({
+		host:process.env["MONGODB_PORT_27017_TCP_ADDR"]||'localhost',
+		port:process.env["MONGODB_PORT_27017_TCP_PORT"]||27017,
+		user:process.env["MONGODB_USERNAME"]||'',
+		pass:process.env["MONGODB_PASSWORD"]||' ',
+		db:process.env["MONGODB_INSTANCE_NAME"]||'test'
+	}))
 	.use(session({store:"cookie"}))
 	.use(logger())
 	.use(gzip())
