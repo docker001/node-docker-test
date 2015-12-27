ed8c75ea706c3f461088159a23612608b8752057
unknown <docker001@github.com>
2015-12-27 19:05:35 +0800

    s2

--- pages/index/index.jsx
+++ pages/index/index.jsx
@@ -2,8 +2,12 @@ import ReactDOM from "react-dom"
 import React,{Component} from "react"
 import {MenuItem,IconMenu,SvgIcon,AppBar,IconButton,FlatButton,List,ListItem,Dialog,TextField,Card} from "material-ui"
 import {ActionBuild,ActionFace,ActionAccessibility,ActionVerifiedUser,SocialGroup,SocialLocationCity,ImageTagFaces,CommunicationCall,ActionCopyright} from "material-ui/lib/svg-icons"
+import {Provider} from "react-redux"
+import {connect} from "react-redux"
 import injectTapEventPlugin from "react-tap-event-plugin"
+import {store,onLoaded} from "./store"
 injectTapEventPlugin()
+@connect(state=>({...state.Config}))
 class Section1 extends Component{
 	render(){return(
 		<section style={{background:"rgba(0,0,0,0.4)",textAlign:"center",height:700,position:"relative",padding:"200px 0 50px 0",color:"#fff"}}>
@@ -15,7 +19,7 @@ class Section1 extends Component{
 			</div>
 			<div style={{height:150}}>
 			</div>
-			<a style={{textDecoration:"none",background:"#3ab0e2",cursor:"pointer",color:"#fff",width:470,display:"inline-block",lineHeight:"40px",verticalAlign:"middle"}} target="_blank" href="http://wpa.qq.com/msgrd?v=3&site=qq&menu=yes">联系我们</a>
+			<a style={{textDecoration:"none",background:"#3ab0e2",cursor:"pointer",color:"#fff",width:470,display:"inline-block",lineHeight:"40px",verticalAlign:"middle"}} target="_blank" href={"http://wpa.qq.com/msgrd?v=3&site=qq&menu=yes&uin="+this.props.qq}>联系我们</a>
 			<div><img style={{float:"right"}} src={require("./QRcode.png")} /></div>
 		</section>
 	)}
@@ -42,29 +46,35 @@ class Section2 extends Component{
 		</section>
 	)}
 }
+@connect(state=>({data:state.News}))
 class Section3 extends Component{
 	render(){return(
 		<section style={{background:"#105153",color:"#fff",padding:"100px 0",textAlign:"center"}}>
 			<div style={{fontSize:40}}>近期新闻</div>
-			{[1,2,3].map(it=>(
+			{(this.props.data||[]).map(it=>(
 				<div key={Math.random()} style={{display:"inline-block",padding:"1.5%",width:"25%",verticalAlign:"top"}}>
-					<div style={{borderBottom:"1px solid",lineHeight:"40px"}}>&nbsp;</div>
-					<div style={{textAlign:"right",color:"#AAA",margin:10}}>&nbsp;</div>
-					<p style={{minHeight:300}}>&nbsp;</p>
+					<div style={{borderBottom:"1px solid",lineHeight:"40px"}}>{it.title}</div>
+					<div style={{textAlign:"right",color:"#AAA",margin:10}}>{it.date}</div>
+					<p style={{minHeight:300}}>{it.content}</p>
 				</div>
 			))}
 			<p><a href="news.html" style={{textDecoration:"none",color:"#fff"}}>----- 获取更多 -----</a></p>
 		</section>
 	)}
 }
+@connect(state=>({...state.Config}),dispatch=>({onLoaded:onLoaded(dispatch)}))
 class Footer extends Component{
+	constructor(props){
+		super(props)
+		if(this.props.onLoaded)this.props.onLoaded()
+	}
 	render(){return(
 		<section style={{background:"#272727",color:"#fff",textAlign:"center"}}>
 			{[
-				{k:"公司地址",v:"···",icon:SocialLocationCity,color:"#34d293"},
-				{k:"关于我们",v:"···",icon:ImageTagFaces,color:"#f7d861"},
-				{k:"联系电话",v:"···",icon:CommunicationCall,color:"#e96656"},
-				{k:"版权所有",v:"···",icon:ActionCopyright,color:"#3ab0e2"}
+				{k:"公司地址",v:this.props.addr,icon:SocialLocationCity,color:"#34d293"},
+				{k:"关于我们",v:this.props.about,icon:ImageTagFaces,color:"#f7d861"},
+				{k:"联系电话",v:this.props.tel,icon:CommunicationCall,color:"#e96656"},
+				{k:"版权所有",v:this.props.copyright,icon:ActionCopyright,color:"#3ab0e2"}
 			].map(it=>(
 				<div key={Math.random()} style={{display:"inline-block",padding:50}}>
 					<it.icon style={{width:60,height:60,fill:it.color}} />
@@ -77,17 +87,19 @@ class Footer extends Component{
 }
 class App extends Component{
 	render(){return(
-		<div style={{background:"url("+require("./bg.jpg")+")",backgroundAttachment:"fixed",fontFamily:"microsoft yahei,simhei"}}>
-			<AppBar iconElementRight={
-				<IconButton iconStyle={{width:30,height:30}} onTouchTap={()=>window.location="/admin.html"}>
-					<ActionBuild/>
-				</IconButton>
-			} style={{background:"#000",padding:"0 100px",position:"fixed",top:0,width:"100%"}} iconElementLeft={<img src={require("./logo.png")}/>} />
-			<Section1/>
-			<Section2/>
-			<Section3/>
-			<Footer/>
-		</div>
+		<Provider store={store}>
+			<div style={{background:"url("+require("./bg.jpg")+")",backgroundAttachment:"fixed",fontFamily:"microsoft yahei,simhei"}}>
+				<AppBar iconElementRight={
+					<IconButton iconStyle={{width:30,height:30}} onTouchTap={()=>window.location="/admin.html"}>
+						<ActionBuild/>
+					</IconButton>
+				} style={{background:"#000",padding:"0 100px",position:"fixed",top:0,width:"100%"}} iconElementLeft={<img src={require("./logo.png")}/>} />
+				<Section1/>
+				<Section2/>
+				<Section3/>
+				<Footer/>
+			</div>
+		</Provider>
 	)}
 }
 ReactDOM.render(<App />,document.querySelector("#app"))
